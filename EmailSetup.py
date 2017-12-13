311f2c33581db5b10428827a009bd630cdf9a4bb
prince <kowserm@mcmaster.com>
2017-12-13 03:27:02 -0500

    Script Updated

--- EmailSetup.py
+++ EmailSetup.py
@@ -1,10 +1,51 @@
 import base64
 import email
 import smtplib
+import socket
+import re
+import dns.resolver
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+def printye(ye):
+    return "yeyeye"
+
+
 def SendEmail(ToEmail, Ticketnum, Name):
+    # check if email syntax is right
+    addressToVerify = ToEmail
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', addressToVerify)
+
+    if match == None:
+        print('Bad Syntax')
+        return "Incorrect Email"
+        #raise ValueError('Bad Syntax')
+
+    # Next we need to get the MX record for the target domain, in order to start the email verification process.
+    records = dns.resolver.query('gmail.com', 'MX')
+    mxRecord = records[0].exchange
+    mxRecord = str(mxRecord)
+
+    # Get local server hostname
+    host = socket.gethostname()
+
+    # SMTP lib setup (use debug level for full output)
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+
+    # SMTP Conversation
+    server.connect(mxRecord)
+    server.helo(host)
+    server.mail('me@domain.com')
+    code, message = server.rcpt(str(addressToVerify))
+    server.quit()
+
+    # Assume 250 as Success
+    if code == 250:
+        print('Success')
+    else:
+        print('Bad')
+        return ("Incorrect Email")
 
     fromaddr = "princekowserr@gmail.com"
     toaddr = ToEmail
@@ -28,6 +69,9 @@ def SendEmail(ToEmail, Ticketnum, Name):
     server.sendmail(fromaddr, toaddr, text)
     server.set_debuglevel(True)  # show communication with the server
     server.quit()
+    return ("Email SENT")
 
 if __name__ == "__main__":
-    SendEmail("patelh11@mcmaster.ca", "20123", "CapPrice")
+
+     for i in range(20):
+        SendEmail("nguyenbh@mcmaster.ca", "20123", "CapPrice")
---
+++ Gui.py
@@ -0,0 +1,59 @@
+from Tkinter import *
+
+import EmailSetup
+
+def set_text(text):
+    T.delete(1.0,END)
+    T.insert(1.0,text)
+    return
+
+def clear_entry(event, entry):
+    entry.delete(0, END)
+
+#setting up application window
+window = Tk()
+window.title("3DRFP_EMAIL_LYONS")
+window.geometry("500x300")
+window.wm_iconbitmap('')
+# background_image=PhotoImage(file="dj.gif")
+# background_label =Label(window, image=background_image)
+#background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+#setting up titles and labels
+Title = Label(window, text="SendEMail", fg="#383a40", font=("Helvetica",18))
+instr1 = Label(window, text= "Enter The Email, Name, and Ticket number", font=("Helvetica", 13))
+entInstr1 = Entry(window, fg="#273746", insertwidth="5")
+
+T = Text(window, height=2, width=10)
+T.pack(side=TOP, padx=(0,0), pady=(20,0))
+T.insert(END, "Email Verifier")
+
+
+#instr2 = Label(window, text= "Enter your working hours", font=("Helvetica", 13))
+#entInstr2 = Entry(window, fg="#273746", insertwidth="5")
+
+#allows entry to have a default text
+entInstr1.insert(0, 'facebook,yahoo,youtube,....')
+#entInstr2.insert(0, 'Ex: 2:30-22:40')
+#that gets erased after a mouse click
+entInstr1.bind("<Button-1>", lambda event: clear_entry(event, entInstr1))
+#entInstr2.bind("<Button-1>", lambda event: clear_entry(event, entInstr2))
+
+#oraganizing the labels
+#Title.pack(pady=(50,40))
+instr1.pack(pady=(70,0))
+entInstr1.pack(fill=X, padx=50)
+#instr2.pack(pady=(20,3))
+#entInstr2.pack()
+#Buttons
+blockbtn=Button(window, text="Send", fg="#000000", font=("Helvetica",10), command=lambda : [EmailSetup.printye(entInstr1.get()),set_text(EmailSetup.printye(1))])
+blockbtn.config(height=3, width=10)
+blockbtn.pack(side=LEFT, padx=(200,6), pady=0)
+
+
+# cancelbt=Button(window, text="Cancel", fg="#000000", font=("Helvetica",10))
+# cancelbt.config(height=3, width=10)
+#cancelbt.pack(side=LEFT )
+
+window.mainloop()
