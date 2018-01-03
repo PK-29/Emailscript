b0efadb71844d5a0b4f636abe2cbe31423ad8e6a
prince <kowserm@mcmaster.com>
2018-01-03 16:19:02 -0500

    Script Updated

--- EmailSetup.py
+++ EmailSetup.py
@@ -6,7 +6,8 @@ import re
 import dns.resolver
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+class color:
+    BOLD = '\033[1m'
 def printye(ye):
     return "yeyeye"
 
@@ -21,31 +22,31 @@ def SendEmail(ToEmail, Ticketnum, Name):
         return "Incorrect Email"
         #raise ValueError('Bad Syntax')
 
-    # Next we need to get the MX record for the target domain, in order to start the email verification process.
-    records = dns.resolver.query('gmail.com', 'MX')
-    mxRecord = records[0].exchange
-    mxRecord = str(mxRecord)
-
-    # Get local server hostname
-    host = socket.gethostname()
-
-    # SMTP lib setup (use debug level for full output)
-    server = smtplib.SMTP()
-    server.set_debuglevel(0)
-
-    # SMTP Conversation
-    server.connect(mxRecord)
-    server.helo(host)
-    server.mail('me@domain.com')
-    code, message = server.rcpt(str(addressToVerify))
-    server.quit()
-
-    # Assume 250 as Success
-    if code == 250:
-        print('Success')
-    else:
-        print('Bad')
-        return ("Incorrect Email")
+    # # Next we need to get the MX record for the target domain, in order to start the email verification process.
+    # records = dns.resolver.query('gmail.com', 'MX')
+    # mxRecord = records[0].exchange
+    # mxRecord = str(mxRecord)
+    #
+    # # Get local server hostname
+    # host = socket.gethostname()
+    #
+    # # SMTP lib setup (use debug level for full output)
+    # server = smtplib.SMTP()
+    # server.set_debuglevel(0)
+    #
+    # # SMTP Conversation
+    # server.connect(mxRecord)
+    # server.helo(host)
+    # server.mail('me@domain.com')
+    # code, message = server.rcpt(str(addressToVerify))
+    # server.quit()
+    #
+    # # Assume 250 as Success
+    # if code == 250:
+    #     print('Success')
+    # else:
+    #     print('Bad')
+    #     return ("Incorrect Email")
 
     fromaddr = "lyons.newmedia@gmail.com"
     toaddr = ToEmail
@@ -54,26 +55,26 @@ def SendEmail(ToEmail, Ticketnum, Name):
     msg['To'] = toaddr
     msg['Subject'] = "3D Print Request - Ready for Pickup"
 
-    body = "Hi "+ Name + """ ,
+    body = "Hi "+ color.BOLD + Name + """ ,
 
-            Good news! The following requested 3D print job has been printed successfully:
+        Good news! The following requested 3D print job has been printed successfully:
 
-            Ticket '#': """ + Ticketnum + """ 
+        """ + "\033[44;33mTicket '#': TICKETNUMBER\033[m" + """"
 
-            Please bring this email and your McMaster ID card with you to the Help Desk in Lyons New Media Centre (Mills Library, 4th floor) to retrieve your item.
+        Please bring this email and your McMaster ID card with you to the Help Desk in Lyons New Media Centre (Mills Library, 4th floor) to retrieve your item.
 
-            You will be required to sign for it, so a proxy cannot come to pick this up for you.
+        You will be required to sign for it, so a proxy cannot come to pick this up for you.
 
-            We will hold this item for no more than 30 days from today's date before it is reclaimed and/or recycled.  If you cannot make it into the Centre due to work/being home etc., please let us know and we can arrange to hold onto it until you can make it in.
+        We will hold this item for no more than 30 days from today's date before it is reclaimed and/or recycled.  If you cannot make it into the Centre due to work/being home etc., please let us know and we can arrange to hold onto it until you can make it in.
 
-            Sincerely,
+        Sincerely,
 
-            Lyons New Media Centre Staff
+        """ + "\033[3mLyons New Media Centre Staff\033[0m" + """
             
-            -- 
-            Lyons New Media Centre
-            4th Floor, Mills Library
-            """
+        --
+        Lyons New Media Centre
+        4th Floor, Mills Library
+        """
 
  
     html = """\
@@ -94,7 +95,7 @@ def SendEmail(ToEmail, Ticketnum, Name):
     server.ehlo()
 
 
-    server.login(fromaddr, base64.b64decode("c3VwZXJtYW4yOQ=="))
+    server.login(fromaddr, base64.b64decode("Q3IzNHQxdjNNMW5kcw="))
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.set_debuglevel(True)  # show communication with the server
@@ -103,4 +104,4 @@ def SendEmail(ToEmail, Ticketnum, Name):
 
 if __name__ == "__main__":
 
-     base64.b64encode("Cr34t1v3M1nds")
+     #print SendEmail("pincekowserr@gmail.com","20026","TEST")
--- SheetAPI.py
+++ SheetAPI.py
@@ -4,15 +4,22 @@ gc = pygsheets.authorize(service_file='authentications/email script-ceb8d0fcd170
 
 # Open spreadsheet and then workseet
 sh = gc.open('test')
-wks = sh.sheet1
+wks = sh[0]
 # Select worksheet by id, index, title.
 #wks = gc.worksheet_by_title("test")
 # Update a cell with value (just to let him know values is updated ;) )
-wks.update_cell('A1', "Hey yak")
-yr=wks.find("yakak")
-row= yr[0].row
+findcode = wks.find("22032")
+row= findcode[0].row
+wks.update_cell('O'+ str(2), "Y")
+
+
+alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in range(26):
+    c = wks.cell(alphabet[i]+str(row))
+    c.color = (.05,.60,1,.1)
 c1 = wks.cell('A15')
-c1.color = (0,0,1,1)
+
+c1.color = (1,0,1,1)
 c1.update()
 c1.unlink()
 #wks.update_cell('A15', Color= 'blue')
